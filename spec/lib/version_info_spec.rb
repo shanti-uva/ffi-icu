11cfc8d758b8143bbf9e76b6e30a842f1a2c70c0
Derik Ramirez <rderik@gmail.com>
2019-04-10 16:20:16 -0600

    Update Rspec and fixed all the tests
    Added Tibetan Collation test and fixed the code for all the tests to
    work with new RSpec 3+

--- spec/lib/version_info_spec.rb
+++ spec/lib/version_info_spec.rb
@@ -5,10 +5,11 @@ require 'spec_helper'
 module ICU
   module Lib
     describe VersionInfo do
-      its(:to_a) { should be_an Array }
-      its(:to_s) do
-        should be_a String
-        should match /^[0-9.]+$/
+      it(:to_a) { expect(subject.to_a).to be_kind_of(Array) }
+      it(:to_s) do
+        s_subject = subject.to_s
+        expect(s_subject).to be_kind_of String
+        expect(s_subject).to match /^[0-9.]+$/
       end
     end
   end
--- spec/locale_spec.rb
+++ spec/locale_spec.rb
@@ -7,25 +7,25 @@ module ICU
     describe 'the available locales' do
       subject { Locale.available }
 
-      it { should be_an Array }
-      it { should_not be_empty }
-      its(:first) { should be_a Locale }
+      it { expect(subject).to be_an Array }
+      it { expect(subject).not_to be_empty }
+      it { expect(subject.first).to be_a Locale }
     end
 
     describe 'the available ISO 639 country codes' do
       subject { Locale.iso_countries }
 
-      it { should be_an Array }
-      it { should_not be_empty }
-      its(:first) { should be_a String }
+      it { expect(subject).to be_an Array }
+      it { expect(subject).not_to be_empty }
+      it { expect(subject.first).to be_a String }
     end
 
     describe 'the available ISO 639 language codes' do
       subject { Locale.iso_languages }
 
-      it { should be_an Array }
-      it { should_not be_empty }
-      its(:first) { should be_a String }
+      it { expect(subject).to be_an Array }
+      it { expect(subject).not_to be_empty }
+      it { expect(subject.first).to be_a String }
     end
 
     describe 'the default' do
@@ -37,44 +37,44 @@ module ICU
         locales.respond_to?(:sample) ? locales.sample : locales.choice
       end
 
-      it { should be_a Locale }
+      it { expect(subject).to be_a Locale }
 
       it 'can be assigned using Locale' do
-        (Locale.default = locale).should == locale
-        Locale.default.should == locale
+        expect(Locale.default = locale).to eq locale
+        expect(Locale.default).to eq locale
       end
 
       it 'can be assigned using string' do
         string = locale.to_s
 
-        (Locale.default = string).should == string
-        Locale.default.should == Locale.new(string)
+        expect(Locale.default = string).to eq string
+        expect(Locale.default).to eq Locale.new(string)
       end
 
       it 'can be assigned using symbol' do
         symbol = locale.to_s.to_sym
 
-        (Locale.default = symbol).should == symbol
-        Locale.default.should == Locale.new(symbol)
+        expect(Locale.default = symbol).to eq symbol
+        expect(Locale.default).to eq Locale.new(symbol)
       end
     end
 
     if Gem::Version.new('4.2') <= Gem::Version.new(Lib.version)
       describe 'BCP 47 language tags' do
         it 'converts a language tag to a locale' do
-          Locale.for_language_tag('en-us').should == Locale.new('en_US')
-          Locale.for_language_tag('nan-Hant-tw').should == Locale.new('nan_Hant_TW')
+          expect(Locale.for_language_tag('en-us')).to eq Locale.new('en_US')
+          expect(Locale.for_language_tag('nan-Hant-tw')).to eq Locale.new('nan_Hant_TW')
         end
 
         it 'returns a language tag for a locale' do
           if Gem::Version.new('4.4') <= Gem::Version.new(Lib.version)
-            Locale.new('en_US').to_language_tag.should == 'en-US'
-            Locale.new('zh_TW').to_language_tag.should == 'zh-TW'
-            Locale.new('zh_Hans_CH_PINYIN').to_language_tag.should == 'zh-Hans-CH-u-co-pinyin'
+            expect(Locale.new('en_US').to_language_tag).to eq 'en-US'
+            expect(Locale.new('zh_TW').to_language_tag).to eq 'zh-TW'
+            expect(Locale.new('zh_Hans_CH_PINYIN').to_language_tag).to eq 'zh-Hans-CH-u-co-pinyin'
           else
-            Locale.new('en_US').to_language_tag.should == 'en-us'
-            Locale.new('zh_TW').to_language_tag.should == 'zh-tw'
-            Locale.new('zh_Hans_CH_PINYIN').to_language_tag.should == 'zh-hans-ch-u-co-pinyin'
+            expect(Locale.new('en_US').to_language_tag).to eq 'en-us'
+            expect(Locale.new('zh_TW').to_language_tag).to eq 'zh-tw'
+            expect(Locale.new('zh_Hans_CH_PINYIN').to_language_tag).to eq 'zh-hans-ch-u-co-pinyin'
           end
         end
       end
@@ -82,41 +82,41 @@ module ICU
 
     describe 'Win32 locale IDs' do
       it 'converts an LCID to a locale' do
-        Locale.for_lcid(1033).should == Locale.new('en_US')
-        Locale.for_lcid(1036).should == Locale.new('fr_FR')
+        expect(Locale.for_lcid(1033)).to eq Locale.new('en_US')
+        expect(Locale.for_lcid(1036)).to eq Locale.new('fr_FR')
       end
 
       it 'returns an LCID for a locale' do
-        Locale.new('en_US').lcid.should == 1033
-        Locale.new('es_US').lcid.should == 21514
+        expect(Locale.new('en_US').lcid).to eq 1033
+        expect(Locale.new('es_US').lcid).to eq 21514
       end
     end
 
     describe 'display' do
       context 'in a specific locale' do
         it 'returns the country' do
-          Locale.new('de_DE').display_country('en').should == 'Germany'
-          Locale.new('en_US').display_country('fr').should == 'États-Unis'
+          expect(Locale.new('de_DE').display_country('en')).to eq 'Germany'
+          expect(Locale.new('en_US').display_country('fr')).to eq 'États-Unis'
         end
 
         it 'returns the language' do
-          Locale.new('fr_FR').display_language('de').should == 'Französisch'
-          Locale.new('zh_CH').display_language('en').should == 'Chinese'
+          expect(Locale.new('fr_FR').display_language('de')).to eq 'Französisch'
+          expect(Locale.new('zh_CH').display_language('en')).to eq 'Chinese'
         end
 
         it 'returns the name' do
-          Locale.new('en_US').display_name('de').should == 'Englisch (Vereinigte Staaten)'
-          Locale.new('zh_CH').display_name('fr').should == 'chinois (Suisse)'
+          expect(Locale.new('en_US').display_name('de')).to eq 'Englisch (Vereinigte Staaten)'
+          expect(Locale.new('zh_CH').display_name('fr')).to eq 'chinois (Suisse)'
         end
 
         it 'returns the script' do
-          Locale.new('ja_Hira_JP').display_script('en').should == 'Hiragana'
-          Locale.new('ja_Hira_JP').display_script('ru').should == 'Хирагана'
+          expect(Locale.new('ja_Hira_JP').display_script('en').downcase).to eq 'hiragana'
+          expect(Locale.new('ja_Hira_JP').display_script('ru').downcase).to eq 'хирагана'
         end
 
         it 'returns the variant' do
-          Locale.new('be_BY_TARASK').display_variant('de').should == 'Taraskievica-Orthographie'
-          Locale.new('zh_CH_POSIX').display_variant('en').should == 'Computer'
+          expect(Locale.new('be_BY_TARASK').display_variant('de')).to eq 'Taraskievica-Orthographie'
+          expect(Locale.new('zh_CH_POSIX').display_variant('en')).to eq 'Computer'
         end
       end
 
@@ -124,23 +124,23 @@ module ICU
         let(:locale) { Locale.new('de_DE') }
 
         it 'returns the country' do
-          locale.display_country.should == locale.display_country(Locale.default)
+          expect(locale.display_country).to eq locale.display_country(Locale.default)
         end
 
         it 'returns the language' do
-          locale.display_language.should == locale.display_language(Locale.default)
+          expect(locale.display_language).to eq locale.display_language(Locale.default)
         end
 
         it 'returns the name' do
-          locale.display_name.should == locale.display_name(Locale.default)
+          expect(locale.display_name).to eq locale.display_name(Locale.default)
         end
 
         it 'returns the script' do
-          locale.display_script.should == locale.display_script(Locale.default)
+          expect(locale.display_script).to eq locale.display_script(Locale.default)
         end
 
         it 'returns the variant' do
-          locale.display_variant.should == locale.display_variant(Locale.default)
+          expect(locale.display_variant).to eq locale.display_variant(Locale.default)
         end
       end
     end
@@ -148,26 +148,26 @@ module ICU
     describe 'formatting' do
       let(:locale) { Locale.new('de-de.utf8@collation = phonebook') }
 
-      it('is formatted') { locale.name.should == 'de_DE.utf8@collation=phonebook' }
-      it('is formatted without keywords') { locale.base_name.should == 'de_DE.utf8' }
-      it('is formatted for ICU') { locale.canonical.should == 'de_DE@collation=phonebook' }
+      it('is formatted') { expect(locale.name).to eq 'de_DE.utf8@collation=phonebook' }
+      it('is formatted without keywords') { expect(locale.base_name).to eq 'de_DE.utf8' }
+      it('is formatted for ICU') { expect(locale.canonical).to eq 'de_DE@collation=phonebook' }
     end
 
     it 'truncates a properly formatted locale, returning the "parent"' do
-      Locale.new('es-mx').parent.should == ''
-      Locale.new('es_MX').parent.should == 'es'
-      Locale.new('zh_Hans_CH_PINYIN').parent.should == 'zh_Hans_CH'
+      expect(Locale.new('es-mx').parent).to eq ''
+      expect(Locale.new('es_MX').parent).to eq 'es'
+      expect(Locale.new('zh_Hans_CH_PINYIN').parent).to eq 'zh_Hans_CH'
     end
 
     describe 'ISO codes' do
       it 'returns the ISO 3166 alpha-3 country code' do
-        Locale.new('en_US').iso_country.should == 'USA'
-        Locale.new('zh_CN').iso_country.should == 'CHN'
+        expect(Locale.new('en_US').iso_country).to eq 'USA'
+        expect(Locale.new('zh_CN').iso_country).to eq 'CHN'
       end
 
       it 'returns the ISO 639 three-letter language code' do
-        Locale.new('en_US').iso_language.should == 'eng'
-        Locale.new('zh_CN').iso_language.should == 'zho'
+        expect(Locale.new('en_US').iso_language).to eq 'eng'
+        expect(Locale.new('zh_CN').iso_language).to eq 'zho'
       end
     end
 
@@ -184,64 +184,64 @@ module ICU
         let(:locale) { Locale.new('de_DE@currency=EUR') }
 
         it 'returns the list of keywords' do
-          locale.keywords.should == ['currency']
+          expect(locale.keywords).to eq ['currency']
         end
       end
 
       it 'can be read' do
-        Locale.new('en_US@calendar=chinese').keyword('calendar').should == 'chinese'
-        Locale.new('en_US@calendar=chinese').keyword(:calendar).should == 'chinese'
-        Locale.new('en_US@some=thing').keyword('missing').should == ''
+        expect(Locale.new('en_US@calendar=chinese').keyword('calendar')).to eq 'chinese'
+        expect(Locale.new('en_US@calendar=chinese').keyword(:calendar)).to eq 'chinese'
+        expect(Locale.new('en_US@some=thing').keyword('missing')).to eq ''
       end
 
       it 'can be added' do
-        Locale.new('de_DE').with_keyword('currency', 'EUR').should == Locale.new('de_DE@currency=EUR')
-        Locale.new('de_DE').with_keyword(:currency, :EUR).should == Locale.new('de_DE@currency=EUR')
+        expect(Locale.new('de_DE').with_keyword('currency', 'EUR')).to eq Locale.new('de_DE@currency=EUR')
+        expect(Locale.new('de_DE').with_keyword(:currency, :EUR)).to eq Locale.new('de_DE@currency=EUR')
       end
 
       it 'can be added using hash' do
-        Locale.new('fr').with_keywords(:a => :b, :c => :d).should == Locale.new('fr@a=b;c=d')
+        expect(Locale.new('fr').with_keywords(:a => :b, :c => :d)).to eq Locale.new('fr@a=b;c=d')
       end
 
       it 'can be removed' do
-        Locale.new('en_US@some=thing').with_keyword(:some, nil).should == Locale.new('en_US')
-        Locale.new('en_US@some=thing').with_keyword(:some, '').should == Locale.new('en_US')
+        expect(Locale.new('en_US@some=thing').with_keyword(:some, nil)).to eq Locale.new('en_US')
+        expect(Locale.new('en_US@some=thing').with_keyword(:some, '')).to eq Locale.new('en_US')
       end
     end
 
     describe 'orientation' do
       it 'returns the character orientation' do
-        Locale.new('ar').character_orientation.should == :rtl
-        Locale.new('en').character_orientation.should == :ltr
-        Locale.new('fa').character_orientation.should == :rtl
+        expect(Locale.new('ar').character_orientation).to eq :rtl
+        expect(Locale.new('en').character_orientation).to eq :ltr
+        expect(Locale.new('fa').character_orientation).to eq :rtl
       end
 
       it 'returns the line orientation' do
-        Locale.new('ar').line_orientation.should == :ttb
-        Locale.new('en').line_orientation.should == :ttb
-        Locale.new('fa').line_orientation.should == :ttb
+        expect(Locale.new('ar').line_orientation).to eq :ttb
+        expect(Locale.new('en').line_orientation).to eq :ttb
+        expect(Locale.new('fa').line_orientation).to eq :ttb
       end
     end
 
     describe 'subtags' do
       let(:locale) { Locale.new('zh-hans-ch-pinyin') }
 
-      it('returns the country code')  { locale.country.should == 'CH' }
-      it('returns the language code') { locale.language.should == 'zh' }
-      it('returns the script code')   { locale.script.should == 'Hans' }
-      it('returns the variant code')  { locale.variant.should == 'PINYIN' }
+      it('returns the country code')  { expect(locale.country).to eq 'CH' }
+      it('returns the language code') { expect(locale.language).to eq 'zh' }
+      it('returns the script code')   { expect(locale.script).to eq 'Hans' }
+      it('returns the variant code')  { expect(locale.variant).to eq 'PINYIN' }
 
       describe 'likely subtags according to UTS #35' do
         it 'adds likely subtags' do
-          Locale.new('en').with_likely_subtags.should == Locale.new('en_Latn_US')
-          Locale.new('sr').with_likely_subtags.should == Locale.new('sr_Cyrl_RS')
-          Locale.new('zh_TW').with_likely_subtags.should == Locale.new('zh_Hant_TW')
+          expect(Locale.new('en').with_likely_subtags).to eq Locale.new('en_Latn_US')
+          expect(Locale.new('sr').with_likely_subtags).to eq Locale.new('sr_Cyrl_RS')
+          expect(Locale.new('zh_TW').with_likely_subtags).to eq Locale.new('zh_Hant_TW')
         end
 
         it 'removes likely subtags' do
-          Locale.new('en_US').with_minimized_subtags.should == Locale.new('en')
-          Locale.new('sr_RS').with_minimized_subtags.should == Locale.new('sr')
-          Locale.new('zh_Hant_TW').with_minimized_subtags.should == Locale.new('zh_TW')
+          expect(Locale.new('en_US').with_minimized_subtags).to eq Locale.new('en')
+          expect(Locale.new('sr_RS').with_minimized_subtags).to eq Locale.new('sr')
+          expect(Locale.new('zh_Hant_TW').with_minimized_subtags).to eq Locale.new('zh_TW')
         end
       end
     end
